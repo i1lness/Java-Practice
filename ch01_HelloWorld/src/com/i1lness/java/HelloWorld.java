d4c6110e319b450f5e6f59f7ba3f0ec6de372fbb
i1lness <shdxrvet@gmail.com>
2022-09-25 07:07:51 +0900

    ch01의 들여쓰기 4칸에서 2칸으로 수정 (2022-09-25)

--- ch01_HelloWorld/src/com/i1lness/java/HelloWorld.java
+++ ch01_HelloWorld/src/com/i1lness/java/HelloWorld.java
@@ -1,7 +1,7 @@
 package com.i1lness.java;
 
-    public class HelloWorld{
-        public static void main(String[] args){
-            System.out.println("Hello World");
-        }
-    }
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
