20f456464595525446f106e1657ea93a596d8be0
i1lness <shdxrvet@gmail.com>
2022-09-28 20:30:22 +0900

    ch10-1 공공 API 기본 / ch10-2 자바 네트워크 API 학습 완료 (2022-09-28)

---
+++ ch10_publicapis/src/main/java/com/i1lness/java/ch10/ApiExplorerV2.java
@@ -0,0 +1,52 @@
+package com.i1lness.java.ch10;
+
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class ApiExplorerV2 {
+  private static final String SERVICEKEY = "NnqbJ8fVmApsj2g3LL6MvXMQBuR6H1ZZkiMJt1tuYHkL1CrJ7xxVlFKQwzOBntZBQ6DR99MsK5k72KWiEAxqbA%3D%3D";
+  private static final String TODAY = new SimpleDateFormat("yyyyMMdd").format(new Date());
+
+  public static void main(String[] args) throws IOException {
+    StringBuilder urlBuilder = new StringBuilder(
+        "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst");
+    urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8")
+        + "=" + SERVICEKEY);
+    urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8"));
+    urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("1000", "UTF-8"));
+    urlBuilder.append("&" + URLEncoder.encode("dataType", "UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8"));
+    urlBuilder.append("&" + URLEncoder.encode("base_date", "UTF-8") + "=" + URLEncoder.encode(TODAY, "UTF-8"));
+    urlBuilder.append("&" + URLEncoder.encode("base_time", "UTF-8") + "=" + URLEncoder.encode("0600", "UTF-8"));
+    urlBuilder.append("&" + URLEncoder.encode("nx", "UTF-8") + "=" + URLEncoder.encode("55", "UTF-8"));
+    urlBuilder.append("&" + URLEncoder.encode("ny", "UTF-8") + "=" + URLEncoder.encode("127", "UTF-8"));
+
+    System.out.println("URL: " + urlBuilder.toString());
+
+    URL url = new URL(urlBuilder.toString());
+    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    conn.setRequestMethod("GET");
+    conn.setRequestProperty("Content-type", "application/json");
+    System.out.println("Response code: " + conn.getResponseCode());
+    BufferedReader rd;
+    if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+      rd = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
+    } else {
+      rd = new BufferedReader(new InputStreamReader(conn.getErrorStream(), StandardCharsets.UTF_8));
+    }
+    StringBuilder sb = new StringBuilder();
+    String line;
+    while ((line = rd.readLine()) != null) {
+      sb.append(line);
+    }
+    rd.close();
+    conn.disconnect();
+    System.out.println(sb.toString());
+  }
+}
